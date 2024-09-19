9f856671804fa799500a654a0c87b60ffe461f2e
gaddamvyshnavi4 <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-09-19 09:28:45 +0530

    Add files via upload

---
+++ Day-5/Using compiler dynamic input.py
@@ -0,0 +1,69 @@
+'''a=2
+b=3
+c=a+b
+print(c)
+print(a+b)
+print(2+4)
+#run_time input()
+
+a=int(input())
+b=int(input())
+print(a+b)
+#if a is 5.6 it gives error since int(input())
+a=complex(input()) #input 4 output 4+0j
+print(a)
+a=float(input())#input 5 output 5.0
+print(a)
+a=str(input('string'))
+print(a)
+
+lname=input()
+fname=input()
+name=(lname+" "+fname).title()
+print(name)
+
+a=complex(input())
+#if input is empty then a is False else True
+b=complex(input())
+print(a+b)
+a=bool(input())
+b=bool(input())
+print(a+b)
+
+print(a)
+print(b)
+print(int(a))
+print(int(b))
+c=True
+d=False
+print(c+d)
+
+a=int(input())
+
+b=int(input())
+c=int(input(''select the option
+                    1.add
+                    2.sub
+                    3.multiply
+                    ''))
+print(a+b)
+
+a=int(input())
+
+b=int(input())
+c=int(input('select the option
+                    1.add
+                    2.sub
+                    3.multiply
+                    4.integer division
+                    5.Float division
+                    6.power                '))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a/b)
+print(a**b)
+
+'''
+
