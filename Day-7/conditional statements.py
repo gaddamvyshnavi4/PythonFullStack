31e2524d004902ab0acc4c56c507476519e6c132
vyshnavigaddam <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-09-23 20:46:28 +0530

    Add files via upload

---
+++ Day-7/conditional statements.py
@@ -0,0 +1,60 @@
+#conditional statements
+#if condition by using comparision operators
+'''
+if 2<3:
+          print('less')
+if 3>2:
+          print('high')
+if 2<=2:
+          print('less than equal to')
+if 6>=3:
+          print('greater than or euqal to')
+if 6==6:
+          print('equal to')
+if 5!=3:
+          print('not equal to')
+
+#using run timw input
+a=int(input())
+b=int(input())
+if a>b:
+          print(a,' is greater than',b)
+if input()=='Vyshnavi':
+          print('equal')
+
+#if statement using logical operators
+if True and True:
+          print('both true')
+if True and not(False):
+          print("True,false")
+if False and False:
+          print("Both False")
+
+if True or True:
+          print('both true')
+if True or False:
+          print("True,false")
+if False or False:
+          print("Both False")
+'''
+'''
+a=1,2,3
+if 1 in a:
+          print('present')
+if [1,2,3] is a:
+          print('memory is not same')
+a=6
+if a is 6:
+          print(6)
+a=[1,2,3]
+b=[5,6,7]
+if a is b:
+          print('same list element')
+a={1:2}
+if 1 in a:
+          print(True)
+          '''
+
+if '1' in list(input()):
+          print('present')
+
