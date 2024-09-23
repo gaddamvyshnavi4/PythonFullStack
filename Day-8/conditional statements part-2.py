887286ea49eb2524ec224077331a7f0748fb46a7
vyshnavigaddam <106810453+gaddamvyshnavi4@users.noreply.github.com>
2024-09-23 20:46:51 +0530

    Add files via upload

---
+++ Day-8/conditional statements part-2.py
@@ -0,0 +1,37 @@
+#if-else
+a=2
+b=3
+if a<b:
+          print('less')
+else:
+          print('greater')
+
+
+if int is type(a):
+          print('its int')
+else:
+          print('not int')
+
+#if else with membersho=ip operator
+if 'i' in 'vyshnavi':
+          print('i is present i vyshnavi')
+else:
+          print('not present')
+#if else with logical operator
+if input('enter a number') and input('enter a number'):
+          print('both are valid')
+else:
+          print('one string is empty')
+
+
+#elif
+
+a=8
+b=10
+if a>b:
+          print('grater')
+elif a<b:
+          print('lesser')
+else:
+          print('equal')
+
